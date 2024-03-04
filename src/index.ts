7bf6009d8c8464110b7f0698c2ee95545cfe59d5
Shawn Hardern <shawnhardern@gmail.com>
2024-03-04 13:26:25 +0000

    Update file formatting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import './index.scss';
 
-document.addEventListener('DOMContentLoaded', function() {
-    addCurrentYearFooter();
+document.addEventListener('DOMContentLoaded', function () {
+	addCurrentYearFooter();
 });
 
-function addCurrentYearFooter () {
-    const footerYear = document.querySelector('.footer__year');
-    if(footerYear) {
-        const currentYear = new Date().getFullYear();
-        footerYear.textContent = currentYear.toString();
-    }
+function addCurrentYearFooter() {
+	const footerYear = document.querySelector('.footer__year');
+	if (footerYear) {
+		const currentYear = new Date().getFullYear();
+		footerYear.textContent = currentYear.toString();
+	}
 }
