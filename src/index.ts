6c2435c17d16119f73cf399e9bb192fd4db435f1
Shawn Hardern <126725649+ShawnHardern@users.noreply.github.com>
2024-03-04 13:51:09 +0000

    Add Footer (#11)
    * Update svg images
    
    * Add footer markup styling and functionality
    
    * Update file formatting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import './index.scss';
 
-console.log('RiftRocket');
+document.addEventListener('DOMContentLoaded', function () {
+	addCurrentYearFooter();
+});
+
+function addCurrentYearFooter() {
+	const footerYear = document.querySelector('.footer__year');
+	if (footerYear) {
+		const currentYear = new Date().getFullYear();
+		footerYear.textContent = currentYear.toString();
+	}
+}
