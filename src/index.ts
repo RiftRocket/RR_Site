77e41f44563c0415455169546fad04a60eeaa596
Ian <ianolivier01@gmail.com>
2024-03-05 11:57:07 +0000

    Add header menu drawer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './index.scss';
+import './scripts/components/header.ts'
 import { Footer } from './scripts/components/footer.ts';
 
 document.addEventListener('DOMContentLoaded', function () {
---
+++ src/scripts/components/header.ts
@@ -0,0 +1,13 @@
+const closeBTN = document.querySelector('.menu__button-close');
+const menuSlider = document.querySelector('.menu__slider');
+const menuBTN = document.querySelector('.header__menu-icon');
+
+menuBTN?.addEventListener('click', function(e){
+    menuSlider.style.width = '15rem';
+})
+
+closeBTN?.addEventListener('click', function(e){
+    menuSlider.style.width = '0';
+})
+
+
