b10d600c09e6ed5e6556229628e3df4aee3b7e9b
Ian <ianolivier01@gmail.com>
2024-03-05 12:11:27 +0000

    Fix convert JS to TS

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -1,13 +1,17 @@
-const closeBTN = document.querySelector('.menu__button-close');
-const menuSlider = document.querySelector('.menu__slider');
-const menuBTN = document.querySelector('.header__menu-icon');
+const closeBTN: HTMLElement | null = document.querySelector('.menu__button-close');
+const menuSlider: HTMLElement | null = document.querySelector('.menu__slider');
+const menuBTN: HTMLElement | null = document.querySelector('.header__menu-icon');
 
-menuBTN?.addEventListener('click', function(){
-    menuSlider.style.width = '15rem';
+menuBTN?.addEventListener('click', function() {
+    if (menuSlider) {
+        menuSlider.style.width = '15rem';
+    }
 });
 
-closeBTN?.addEventListener('click', function(){
-    menuSlider.style.width = '0';
+closeBTN?.addEventListener('click', function() {
+    if (menuSlider) {
+        menuSlider.style.width = '0';
+    }
 });
 
 
