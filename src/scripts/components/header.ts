3709a531e8afde5a42990b463354280e4fe84f43
Ian <ianolivier01@gmail.com>
2024-03-05 12:02:29 +0000

    Fix linting errors

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -2,12 +2,12 @@ const closeBTN = document.querySelector('.menu__button-close');
 const menuSlider = document.querySelector('.menu__slider');
 const menuBTN = document.querySelector('.header__menu-icon');
 
-menuBTN?.addEventListener('click', function(e){
+menuBTN?.addEventListener('click', function(){
     menuSlider.style.width = '15rem';
-})
+});
 
-closeBTN?.addEventListener('click', function(e){
+closeBTN?.addEventListener('click', function(){
     menuSlider.style.width = '0';
-})
+});
 
 
