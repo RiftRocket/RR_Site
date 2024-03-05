6e211c3e38947fe8aa65f9a93aad7fa619280fe5
Ian <ianolivier01@gmail.com>
2024-03-05 12:16:02 +0000

    Fix final linting fix

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -3,15 +3,13 @@ const menuSlider: HTMLElement | null = document.querySelector('.menu__slider');
 const menuBTN: HTMLElement | null = document.querySelector('.header__menu-icon');
 
 menuBTN?.addEventListener('click', function() {
-    if (menuSlider) {
-        menuSlider.style.width = '15rem';
-    }
+	if (menuSlider) {
+    	menuSlider.style.width = '15rem';
+	}
 });
 
 closeBTN?.addEventListener('click', function() {
-    if (menuSlider) {
-        menuSlider.style.width = '0';
-    }
+	if (menuSlider) {
+    	menuSlider.style.width = '0';
+	}
 });
-
-
