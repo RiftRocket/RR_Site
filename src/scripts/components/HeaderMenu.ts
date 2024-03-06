d8de764f6e3fc97c2f9d6671bd2566b442d4f49e
Shawn Hardern <shawnhardern@gmail.com>
2024-03-06 23:26:39 +0000

    Add wip

--- src/scripts/components/HeaderMenu.ts
+++ src/scripts/components/HeaderMenu.ts
@@ -1,12 +1,14 @@
 export class HeaderMenu {
-	private readonly menuSlider: Element | null;
-	private readonly menuSliderCloseButton: Element | null;
-	private readonly menuButton: Element | null;
+	private readonly menuSlider: HTMLElement | null;
+	private readonly menuSliderCloseButton: HTMLElement | null;
+	private readonly menuButton: HTMLElement | null;
+	private readonly menuNav: HTMLElement | null;
 
 	constructor() {
 		this.menuSliderCloseButton = document.querySelector('.header-menu__button-close');
 		this.menuSlider = document.querySelector('.header-menu__slider');
 		this.menuButton = document.querySelector('.header__menu-icon');
+		this.menuNav = document.querySelector('.header-menu__slider-item-container');
 
 		this.menuSlider?.setAttribute('aria-expanded',"false");
 
@@ -25,8 +27,14 @@ export class HeaderMenu {
 
 	toggleMenu() {
 		if (this.menuSlider) {
-			const IsAriaExpanded = this.menuSlider.getAttribute('aria-expanded') === "true";
 			this.menuSlider.classList.toggle('header-menu__slider--active');
+			let hasActiveClass = this.menuSlider.classList.contains('header-menu__slider--active');
+
+			if(hasActiveClass) {
+				this.menuNav?.focus();
+			}
+
+			const IsAriaExpanded = this.menuSlider.getAttribute('aria-expanded') === "true";
 			this.menuSlider.setAttribute('aria-expanded',`${!IsAriaExpanded}`)
 
 		}
